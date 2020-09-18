9fffee56d8e4b95023dde0793a6cbf9ff14a26e3
miqbal-redhat <miqbal@redhat.com>
2020-09-18 12:15:17 -0700

    Update configure_custom_config.sh

--- bin/configure_custom_config.sh
+++ bin/configure_custom_config.sh
@@ -15,7 +15,7 @@ function swapVars() {
   sed -i "s/\${AMQ_STORAGE_USAGE_LIMIT}/$AMQ_STORAGE_USAGE_LIMIT/g" $1
   sed -i "s/\${AMQ_CLUSTER_USER}/$AMQ_CLUSTER_USER/g" $1
   sed -i "s/\${AMQ_CLUSTER_PASSWORD}/$AMQ_CLUSTER_PASSWORD/g" $1
-  sed -i "s/\${AMQ_KEYSTORE_TRUSTSTORE_DIR}/$BROKER_IP/g" $1
+  sed -i "s/\${SSL_DIR}/$SSL_DIR/g" $1
 
 }
 
