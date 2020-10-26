e86739075a0bc57bc2199498ad530d7c8b749755
miqbal-redhat <miqbal@redhat.com>
2020-10-26 13:59:06 -0700

    Update configure_s2i_files.sh

--- bin/configure_s2i_files.sh
+++ bin/configure_s2i_files.sh
@@ -6,13 +6,5 @@ echo "Copying Config files from S2I build"
 cp -v $AMQ_HOME/conf/* ${INSTANCE_DIR}/etc/
 cp -v /etc/amq/ext/etc/* ${INSTANCE_DIR}/etc/
 
-export -Dcom.sun.management.jmxremote=true 
-export -Dcom.sun.management.jmxremote.authenticate=false  
-export -Dcom.sun.management.jmxremote.ssl=false  
-export -Dcom.sun.management.jmxremote.local.only=false 
-export -Dcom.sun.management.jmxremote.port=1099  
-export -Dcom.sun.management.jmxremote.rmi.port=1099  
-export -Djava.rmi.server.hostname=${BROKER_IP}
-
 #echo "Configuring S2I run to start"
 #sed -i 's/launch\.sh/launch\.sh start/' /usr/local/s2i/run
