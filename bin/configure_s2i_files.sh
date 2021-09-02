8ddbb7b6daeeadd752828c2333a919d8b636942d
miqbal-redhat <miqbal@redhat.com>
2021-09-02 08:26:27 -0700

    Update configure_s2i_files.sh

--- bin/configure_s2i_files.sh
+++ bin/configure_s2i_files.sh
@@ -4,7 +4,7 @@ set -e
 INSTANCE_DIR=$1
 echo "Copying Config files from S2I build"
 cp -v $AMQ_HOME/conf/* ${INSTANCE_DIR}/etc/
-cp -v /etc/amq/ext/etc/* ${INSTANCE_DIR}/etc/
+cp -v /etc/amq/ext/etc/* ${INSTANCE_DIR}/etc
 
 #echo "Configuring S2I run to start"
 #sed -i 's/launch\.sh/launch\.sh start/' /usr/local/s2i/run
